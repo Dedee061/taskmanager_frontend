07c9e6234798f2a5a51beae94b789df71441a34f
Andre Santos <andresantoss0598@gmail.com>
2024-12-02 13:37:07 -0300

    feat: add custom button component

---
+++ src/components/CustomButton.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./CustomButton.scss";
+export default function CustomButton({ onClick, children }) {
+  return (
+    <div className="custom-button-container" onClick={onClick}>
+      {children}
+    </div>
+  );
+}
