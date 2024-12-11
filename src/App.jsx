adc5ba6b09975cec00337b644af8c34e8a9862f2
Andre Santos <andresantoss0598@gmail.com>
2024-12-11 14:25:32 -0300

    feat: add react-alert setuo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import { useEffect } from "react";
 import "./App.css";
-import axios from "axios";
 import Tasks from "./components/tasks";
-import TaskItem from "./components/TaskItem";
+import { transitions, positions, Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
+const options = {
+  // you can also just use 'bottom center'
+  position: positions.BOTTOM_CENTER,
+  timeout: 2000,
+  offset: "30px",
+  // you can also just use 'scale'
+  transition: transitions.SCALE,
+};
 
 function App() {
-  return <Tasks />;
+  return (
+    <AlertProvider template={AlertTemplate} {...options}>
+      <Tasks />
+    </AlertProvider>
+  );
 }
 
 export default App;
