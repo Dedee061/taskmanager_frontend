26a4d26f7aff5a05776b5aa24105735e4be007cd
Andre Santos <andresantoss0598@gmail.com>
2024-12-11 14:39:08 -0300

    feat: add function to add a task

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -2,12 +2,37 @@ import React, { useState } from "react";
 import CustomIput from "./CustomInput";
 import "./AddTask.scss";
 import CustomButton from "./CustomButton";
+import { FaPlus } from "react-icons/fa";
+import axios from "axios";
+import { useAlert } from "react-alert";
 
 export default function AddTask() {
   const [task, setTask] = useState("");
+  const alert = useAlert();
+
   const onChange = (e) => {
     setTask(e.target.value);
   };
+  async function handlerAddTask() {
+    try {
+      if (task === "") {
+        alert.error("Tarefa não pode ser vazia");
+        return;
+      }
+      await axios
+        .post("http://localhost:8000/tasks", {
+          description: task,
+          isCompleted: false,
+        })
+        .then(() => {
+          alert.success("Tarefa adicionada com sucesso");
+          setTask("");
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <div className="add-task-container">
       <CustomIput
@@ -15,7 +40,9 @@ export default function AddTask() {
         value={task}
         onChange={onChange}
       />
-      <CustomButton>+</CustomButton>
+      <CustomButton onClick={handlerAddTask}>
+        <FaPlus size={14} color="#fff" />
+      </CustomButton>
     </div>
   );
 }
